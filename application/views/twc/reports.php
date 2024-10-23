834b3a2df02c6cc1236d9289445e8c06a18a879d
nicko <nickozeusfagarin@gmail.com>
2024-10-23 14:07:22 +0800

    twc reports

--- application/views/twc/reports.php
+++ application/views/twc/reports.php
@@ -25,63 +25,56 @@
                         <h3 class="card-title">Applicants Report by Program</h3>
                     </div>
                     <div class="card-body">
-                        <table id="example3" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Total Applicants</th>
-                                    <th>Qualified</th>
-                                    <th>Not Qualified</th>
-                                    <th>Conditional</th>
-                                    <th>Pending</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><?= $report_counts['total'] ?></td>
-                                    <td><?= $report_counts['qualified'] ?></td>
-                                    <td><?= $report_counts['not_qualified'] ?></td>
-                                    <td><?= $report_counts['conditional'] ?></td>
-                                    <td><?= $report_counts['pending'] ?></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="table-responsive">
+                            <table id="example1" class="table table-bordered table-hover table-sm">
+                                <thead>
+                                    <tr>
+                                        
+                                        <th>ID Number</th>
+                                        <th>Last Name</th>
+                                        <th>First Name</th>
+                                        <th>Scholarship Program</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php if (!empty($applicants)): ?>
+                                        <?php foreach ($applicants as $applicant): ?>
+                                            <tr>
+                                                <td><?= $applicant->id_number; ?></td>
+                                                <td><?= $applicant->lastname; ?></td>
+                                                <td><?= $applicant->firstname; ?></td>
+                                                <td><?= $applicant->scholarship_program; ?></td>
+                                                <td class="status-column"><?= ucwords($applicant->status); ?></td>
+                                                <td>
+                                                    <a href="<?= site_url('twc/view_applicant/' . $applicant->applicant_no); ?>" class="btn btn-info btn-sm">
+                                                        <i class="fas fa-eye"></i>
+                                                    </a>
+                                                    <button class="btn btn-primary evaluate-btn btn-sm" data-id="<?= $applicant->applicant_no; ?>" data-toggle="modal" data-target="#evaluateModal">
+                                                        <i class="fas fa-pencil-alt"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    <?php else: ?>
+                                        <tr>
+                                            <td colspan="8" class="text-center">No data found</td>
+                                        </tr>
+                                    <?php endif; ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="row report-section scholarship-report">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Scholarship Grants</h3>
-                    </div>
-                    <div class="card-body">
-                        <table id="example4" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Program Code</th>
-                                    <th>Scholarship Program</th>
-                                    <th>Percentage</th>
-                                    <th>Number of Grantees</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($scholarship_programs as $program): ?>
-                                    <tr>
-                                        <td><?= $program['program_code'] ?></td>
-                                        <td><?= $program['scholarship_program'] ?></td>
-                                        <td><?= $program['percentage'] ?></td>
-                                        <td><?= $program['num_grantees'] ?></td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </section>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
 <?php $this->load->view('includes/footer'); ?>
